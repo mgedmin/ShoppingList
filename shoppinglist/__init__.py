56c60248679f5007986977d470b7b2e91dc4f4e8
Marius Gedminas <marius@gedmin.as>
2019-02-02 00:58:21 +0200

    Move mako.imports configuration into Python code

--- shoppinglist/__init__.py
+++ shoppinglist/__init__.py
@@ -9,7 +9,10 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, "sqlalchemy.")
     DBSession.configure(bind=engine)
     config = Configurator(settings=settings)
-    config.include('pyramid_mako')
+    config.add_settings(
+        {"mako.imports": "from shoppinglist.filters import json"}
+    )
+    config.include("pyramid_mako")
     config.add_static_view("static", "static", cache_max_age=3600)
     config.add_route("home", "/")
     # Let's go nuts with REST, why don't we?
